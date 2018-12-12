862755c5f3669dbfc43bd0b6a2bf6d1d05e56224
Geetesh Gupta <gg@Geeteshs-MacBook-Air.local>
2018-12-12 15:49:49 +0530

    Removed escape sequence error

--- src/ta_portal/urls.py
+++ src/ta_portal/urls.py
@@ -24,11 +24,11 @@ admin.site.site_title = settings.ADMIN_SITE_TITLE
 admin.site.index_title = settings.ADMIN_INDEX_TITLE
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('frontend.urls')),
-    path('password_reset/', PasswordResetView.as_view(from_email=settings.SERVER_EMAIL), name='password_reset'),
-    path('password_reset/done/', auth_views.password_reset_done, name='password_reset_done'),
-    path('reset/<uidb64>[0-9A-Za-z_\-]+/<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20}/',
+    path(r'^$', include('frontend.urls')),
+    path(r'admin/$', admin.site.urls),
+    path(r'password_reset/$', PasswordResetView.as_view(from_email=settings.SERVER_EMAIL), name='password_reset'),
+    path(r'password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    path(r'reset/<uidb64>[0-9A-Za-z_\-]+/<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20}/$',
          auth_views.password_reset_confirm, name='password_reset_confirm'),
-    path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
+    path(r'reset/done$/', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
